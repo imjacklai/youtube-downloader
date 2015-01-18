695996add7986f908c89f003ad49387b502b05bd
Jack Lai <imjacklai@gmail.com>
2015-01-18 21:05:29 +0800

    use semantic-ui framework instead of bootstrap

--- js/script.js
+++ js/script.js
@@ -1,87 +1,74 @@
 var process = require('child_process');
-var media = null;
-var video_format = null;
-var audio_format = null;
-var options = { 'audio': ["-x"], 'video': [], 'both': ["-k", "-x"] };
 var process_queue = new Array();
-var max_process = 4;
+var max_process = 2;
 var current_process_count = 0;
+var argus = null;
 
-$('input:radio').change(function() {
-  media = $('input[type="radio"][name="media"]:checked').val()
-  if (media === 'audio') {
-    $('select#audio-format').prop({'disabled': false});
-    $('select#video-format').prop({'disabled': 'disabled', 'value': 'null'});
-    video_format = null;
-  }
-  else if (media === "video") {
-    $('select#audio-format').prop({'disabled': 'disabled', 'value': 'null'});
-    $('select#video-format').prop({'disabled': false});
-    audio_format = null;
-  }
-  else {
-    $('select#audio-format').prop({'disabled': false});
-    $('select#video-format').prop({'disabled': false});
-  }
-});
-
-$('select#video-format').change(function() {
-  video_format = $('select#video-format option:selected').val();
-  $('#download-btn').prop({'disabled': false});
-});
-
-$('select#audio-format').change(function() {
-  audio_format = $('select#audio-format option:selected').val();
-  $('#download-btn').prop({'disabled': false});
-});
+$('.dropdown').dropdown({ action: 'activate' });
+$('.dropdown, #path-chooser').popup();
 
 $('#path-chooser').click(function() {
   var chooser = $('#path-dialog');
   chooser.change(function(event) {
-    $("#file-path").val($(this).val());
+    $('#path-chooser').data('content', $(this).val());
   });
   chooser.trigger('click');
 });
 
-$('#download-btn').click(function() {
-  var url = $('input#youtube-url').val();
-  if (url == '') return;
-  $('input#youtube-url').val("");
-  var argus = options[media].slice();
+$('#youtube-url').keypress(function(event) {
+  if(event.keyCode == '13') {
+    var url = $(this).val();
+    if (url == '') return;
+    $('#youtube-url').val('');
 
-  if (video_format !== null) {
-    argus.push("-f", video_format);
+    argus = [].slice();
+    var video_format = $('#video-format').dropdown('get text');
+    var audio_format = $('#audio-format').dropdown('get text');
+    
+    if (video_format != '影片格式' && video_format != '不下載' && (audio_format == '音樂格式' || audio_format == '不下載')) {
+      argus.push("-f", video_format);
+    }
+    else if ((video_format == '影片格式' || video_format == '不下載') && audio_format != '音樂格式' && audio_format != '不下載') {
+      argus.push("-x", "--audio-format", audio_format);
+    }
+    else if (video_format != '影片格式' && video_format != '不下載' && audio_format != '音樂格式' && audio_format != '不下載') {
+      argus.push("-k", "-x", "-f", video_format, "--audio-format", audio_format);
+    }
+    else {
+      return;
+    }
+
+    argus.push("-o", $('#path-chooser').data('content') + "/%(title)s.%(ext)s", url);
+    get_title(argus);
   }
-  if (audio_format !== null) {
-    argus.push("--audio-format", audio_format);
-  }
-
-  argus.push("-o", $("#file-path").val() + "/%(title)s.%(ext)s", url);
-
-  get_title(argus);
 });
 
 function get_title(argus) {
+  $('div.icon.input').removeClass('error').addClass('loading');
+  $('#youtube-url').prop('disabled', 'disabled').prop('placeholder', '貼上youtube影片網址，並按下enter開始下載');
   var url = argus[argus.length - 1];
   var id = url.split("=")[1];
-
-  var progressbar = "<p id='title-" + id + "' class='align-left'>標題擷取中...</p><div class='progress'><div id='" + id + "' class='progress-bar' role='progressbar' data-transitiongoal=''></div></div>";
-  $(".container").append(progressbar);
 
   // Get video's title
   process.exec("youtube-dl -e " + url, { encoding: 'utf8' }, function(error, stdout, stderr) {
     if (error !== null) {
-      $('p#title-' + id).text(url + " 這不是正確的網址");
+      $('div.icon.input').removeClass('loading').addClass('error');
+      $('#youtube-url').prop('disabled', false).prop('placeholder', '這不是正確的網址');
     }
     else {
-      $('p#title-' + id).text(stdout);
-
-      if (current_process_count < max_process) {
+      $('div.icon.input').removeClass('loading');
+      $('#youtube-url').prop('disabled', false);
+      var progressbar = "<div id='" + id + "' class='ui progress'> \
+                           <span>" + stdout + "</span> \
+                           <div class='bar'> \
+                             <div class='progress'></div> \
+                           </div> \
+                         </div>";
+      $(".container").append(progressbar);
+      if (current_process_count < max_process)
         download(argus);
-      }
-      else {
+      else
         process_queue.push(argus);
-      }
     }
   });
 }
@@ -89,24 +76,21 @@ function get_title(argus) {
 function download(argus) {
   var url = argus[argus.length - 1];
   var id = url.split("=")[1];
+  current_process_count += 1;
 
   var child = process.spawn("youtube-dl", argus);
-
-  current_process_count += 1;
-  console.log("a process run, total:" + current_process_count);
-
   child.stdout.on('data', function (data) {
     var value = parseInt($.trim(data).split(" ")[2].split("%")[0]);
-    $("#" + id).attr('data-transitiongoal', value).progressbar({display_text: 'fill'});
+    if (value >= 0)
+      $('#' + id).progress({ percent: value });
   });
 
   child.stderr.on('data', function (data) {
   });
 
   child.on('exit', function (code) {
-    $("#" + id).attr('data-transitiongoal', 100).progressbar({display_text: 'fill'});
+    $('#' + id).progress({ percent: 100 });
     current_process_count -= 1;
-    console.log("a process exit, total:" + current_process_count);
     if (current_process_count < max_process && process_queue.length != 0) {
       download(process_queue.shift())
     }
